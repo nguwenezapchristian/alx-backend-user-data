2451ca1edad5db2fa139bc5caf724c4b21163f51
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-05-29 16:59:53 +0200

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import logging
 import re
 """
 Log message obfuscated to protect personal data
@@ -14,3 +15,20 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = r'({})=[^{}]*'.format('|'.join(map(re.escape, fields)),
                                     re.escape(separator))
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            original_message, self.SEPARATOR)
