d1f7b3e7c769d14ae619a7d0b464edd4c7839985
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-05-29 17:36:07 +0200

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,6 +10,10 @@ import re
 from typing import List
 
 
+"""The fields from user_data.csv that are considered PII"""
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     pattern = r'({})=[^{}]*'.format('|'.join(map(re.escape, fields)),
@@ -32,3 +36,17 @@ class RedactingFormatter(logging.Formatter):
         original_message = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             original_message, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """ Function to get a configured logger """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
