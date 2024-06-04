7c84be2eba04fde10c92d9f47f25344c0fba2311
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-04 19:31:16 +0200

    decode authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 a class BasicAuth that inherits from Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -30,3 +31,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ A method that returns the decoded value of a Base64 string """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_auth = base64.b64decode(base64_authorization_header)
+            return decoded_auth.decode("utf-8")
+        except (ValueError, UnicodeDecodeError):
+            return None
