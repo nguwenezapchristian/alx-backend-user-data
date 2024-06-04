7a84f33cf8679984ec215b5c5423834a9dfdb3e9
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-04 16:56:09 +0200

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+a class BasicAuth that inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ a class BasicAuth that inherits from Auth """
+    pass
