521e35c2a32f4894ebeb2cd2a9cff66f8aba0e82
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-04 20:43:06 +0200

    returns the User instance         based on his email and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ a class BasicAuth that inherits from Auth
 """
 from api.v1.auth.auth import Auth
 import base64
+from models.base import Base
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -58,3 +60,22 @@ class BasicAuth(Auth):
             return (None, None)
         email, password = decoded_base64_authorization_header.split(":")
         return (email, password)
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """ A method that returns the User instance
+        based on his email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        users = User.all()
+        if not users:
+            return None
+        user = User.search({"email": user_email})
+        if not user:
+            return None
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
