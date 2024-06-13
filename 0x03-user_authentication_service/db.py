7941705b904917d7c05265be3ea8a0d11b6958f0
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-14 01:25:15 +0200

    fixed pycodestyle

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -11,9 +11,10 @@ from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
+
 class DB:
     """DB class"""
-    
+
     def __init__(self) -> None:
         """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=True)
