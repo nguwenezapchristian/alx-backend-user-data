a634ea97c3e52825f0aecfb219ae72c1168535e7
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-14 18:49:59 +0200

    removed _hash_password from the class Auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,11 @@ from bcrypt import gensalt, hashpw
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(self, password: str) -> bytes:
+        """Hash a password for storing."""
+        return hashpw(password.encode(), gensalt())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -11,14 +16,9 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def _hash_password(self, password: str) -> bytes:
-        """Hash a password for storing."""
-        return hashpw(password.encode(), gensalt())
-
     def register_user(self, email: str, password: str) -> User:
         """Register a user."""
         if self._db.find_user_by(email=email) is not None:
             raise ValueError(f"User {email} already exists")
 
-        hashed_password = self._hash_password(password)
-        return self._db.add_user(email, hashed_password)
+        return self._db.add_user(email, _hash_password(self, password))
