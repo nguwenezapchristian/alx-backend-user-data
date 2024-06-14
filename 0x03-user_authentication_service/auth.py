f87b49c52851e6ed7f40cebb71a9191d0bba2e6a
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-14 18:36:49 +0200

    register a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,10 +1,31 @@
-#!/usr/bin/env python3
-"""
-Auth module
-"""
+from db import DB
+from user import User
 from bcrypt import gensalt, hashpw
+from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """Hash a password for storing."""
-    return hashpw(password.encode(), gensalt())
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """Hash a password for storing."""
+        return hashpw(password.encode(), gensalt())
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user."""
+        try:
+            if self._db.find_user_by(email=email) is not None:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            """No user found, proceed to register """
+            pass
+
+        hashed_password = self._hash_password(password)
+        user = User(email=email, hashed_password=hashed_password)
+        self._db.add_user(email, hashed_password)
+
+        return user
