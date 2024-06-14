c6d633f0adf48a0cc2e2f3d16ff4bc8f9dc21e79
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-14 23:43:21 +0200

    added get_user_from_session_id and destroy_session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,3 +53,17 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+    
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get a user from a session ID."""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+    
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy a session for a user."""
+        self._db.update_user(user_id, session_id=None)
