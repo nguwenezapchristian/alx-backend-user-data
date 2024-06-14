f261a3b83c8cbcd4f06ede838d8d7c9520f85fad
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-14 19:46:28 +0200

    The end-point to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,14 +2,29 @@
 """Flask App"""
 from flask import Flask
 from flask.json import jsonify
+from auth import Auth
+from flask import request
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def index():
     """Index"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """Users"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except Exception as e:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
