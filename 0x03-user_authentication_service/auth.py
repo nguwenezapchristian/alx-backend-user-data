7bcdcf5d42a4c7fff5c9882a987692e68f77a3d4
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-06-14 23:43:53 +0200

    fixed pycodestyle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,7 +53,7 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
-    
+
     def get_user_from_session_id(self, session_id: str) -> User:
         """Get a user from a session ID."""
         if session_id is None:
@@ -63,7 +63,7 @@ class Auth:
             return user
         except NoResultFound:
             return None
-    
+
     def destroy_session(self, user_id: int) -> None:
         """Destroy a session for a user."""
         self._db.update_user(user_id, session_id=None)
