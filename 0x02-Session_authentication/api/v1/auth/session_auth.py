5a0a0efcdc7ad7e8938063138bee68b7e23d367f
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-07-24 14:11:11 +0200

    Logout

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -42,8 +42,7 @@ class SessionAuth(Auth):
         session_id = self.session_cookie(request)
         if session_id is None:
             return False
-        user_id = self.user_id_for_session_id(session_id)
-        if user_id is None:
+        if self.user_id_for_session_id(session_id) is None:
             return False
         del self.user_id_by_session_id[session_id]
         return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -35,7 +35,8 @@ def login():
     return response
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
 def logout():
     """Logout session"""
     from api.v1.app import auth
