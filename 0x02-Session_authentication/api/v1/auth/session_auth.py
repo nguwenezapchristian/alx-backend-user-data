584bc9d5ccb8401e9bfd431d401393a880b30ed8
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-07-24 13:28:29 +0200

    handles invalid sessions properly and returns the correct status codes

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
+""" SessionAuth module
 """
-Session authentication module
-"""
-from api.v1.auth.auth import Auth
 import uuid
+from flask import request
+from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
-    """SessionAuth class"""
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a session ID for a user_id"""
+        """ Creates a session ID for a user_id """
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
@@ -19,7 +19,29 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns the User ID based on Session ID"""
+        """ Returns a user ID based on a session ID """
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns a User instance based on a cookie value """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ Deletes the user session / logout """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        if session_id not in self.user_id_by_session_id:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
